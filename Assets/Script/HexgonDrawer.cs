8714cfe2695bea53874ed5407e20f7f22db823d8
wxamlzm <zd5024@163.com>
2025-03-08 07:50:39 +0800

    feat: 更新六边形

--- Assets/Script/HexgonDrawer.cs
+++ Assets/Script/HexgonDrawer.cs
@@ -55,4 +55,38 @@ public class HexagonDrawer : MonoBehaviour
         }
     }
 
+    void CreateHexagonMesh()
+    {
+        MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
+        MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
+
+        // 创建一个新的网格
+        Mesh mesh = new Mesh();
+        Vector3[] vertices = new Vector3[7];
+
+        for(int i = 0; i < 6; i++)
+        {
+            float angle = i * 60 * Mathf.Deg2Rad;
+            vertices[i] = new Vector3(Mathf.Cos(angle) * hexRadius, 0, Mathf.Sin(angle) * hexRadius);
+        }
+
+        vertices[6] = Vector3.zero; // 中心点
+
+        int[] trangles = new int[] {
+            0,1,6,
+            1,2,6,
+            2,3,6,
+            3,4,6,
+            4,5,6,
+            5,0,6
+        };
+
+        mesh.vertices = vertices;
+        mesh.triangles = trangles;
+
+        meshFilter.mesh = mesh;
+
+        
+    }
+
 }
